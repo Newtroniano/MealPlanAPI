27280b0c586201b38564a63b088b627e015bfd20
unknown <luisocara.luis@gmail.com>
2025-06-02 19:56:19 -0300

    Db Context class

--- MealPlanAPI/Data/AppDbContext.cs
+++ MealPlanAPI/Data/AppDbContext.cs
@@ -1,8 +1,21 @@
-﻿namespace MealPlanAPI.Data
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : IdentityDbContext<ApplicationUser>
 {
-    public class AppDbContext
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Food> Foods { get; set; }
+    public DbSet<MealPlan> MealPlans { get; set; }
+    public DbSet<MealPlanFood> MealPlanFoods { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
 
+        modelBuilder.Entity<Patient>().HasQueryFilter(p => !p.IsDeleted);
 
+        
     }
 }
