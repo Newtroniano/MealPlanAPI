568633d0fbd10b705849938ec58641f675a5c82e
unknown <luisocara.luis@gmail.com>
2025-06-02 19:41:44 -0300

    creating entities classes

--- MealPlanAPI/Models/Entities/Food.cs
+++ MealPlanAPI/Models/Entities/Food.cs
@@ -1,6 +1,9 @@
-﻿namespace MealPlanAPI.Models.Entities
+﻿public class Food : BaseEntity
 {
-    public class Food
-    {
-    }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double CaloriesPer100g { get; set; }
+    public double ProteinPer100g { get; set; }
+    public double CarbsPer100g { get; set; }
+    public double FatPer100g { get; set; }
 }
--- MealPlanAPI/Models/Entities/MealPlanFood.cs
+++ MealPlanAPI/Models/Entities/MealPlanFood.cs
@@ -1,6 +1,13 @@
-﻿namespace MealPlanAPI.Models.Entities
+﻿public class MealPlanFood
 {
-    public class MealPlanFood
+    public int MealPlanId { get; set; }
+    public MealPlan MealPlan { get; set; }
+    public int FoodId { get; set; }
+    public Food Food { get; set; }
+    public double PortionSizeInGrams { get; set; }
+
+    public double CalculateCalories()
     {
+        return (Food.CaloriesPer100g / 100) * PortionSizeInGrams;
     }
 }
